7dbe7236be9aec3c386b614a8fc6dcf5ec9cadce
kashvi <kashvi.sachdeva@runo.ai>
2025-11-26 18:56:50 +0530

    secret

--- sheet_utils.py
+++ sheet_utils.py
@@ -1,13 +1,17 @@
-import streamlit as st
-from google.oauth2.service_account import Credentials
 
-SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
+import json
+from google.oauth2.service_account import Credentials
+import streamlit as st
+
+SERVICE_INFO = st.secrets["gcp_service"]
+
+creds = Credentials.from_service_account_info(
+    SERVICE_INFO,
+    scopes=["https://www.googleapis.com/auth/spreadsheets"]
+)
 
 def get_sheets_client():
-    creds = Credentials.from_service_account_info(
-        st.secrets["gcp_service"],
-        scopes=SCOPES
-    )
-
     from googleapiclient.discovery import build
-    return build("sheets", "v4", credentials=creds)
+
+    service = build('sheets', 'v4', credentials=creds)
+    return service
